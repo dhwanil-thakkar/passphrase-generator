60720e11ccfb9dec1919c82af009e37864b542c1
Dhwanil Thakkar <59126796+dhwanil-thakkar@users.noreply.github.com>
2024-10-07 16:27:37 -0700

    added Crypotographically secure random number generation

--- generator.py
+++ generator.py
@@ -1,52 +1,33 @@
 import typing
 import random
 import logging
-
+import secrets
 
 
 def setup_logger():
-    logger = logging.Logger(__file__)
-    file_handler = logging.FileHandler("log.txt")
+    logger = logging.Logger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler("generator_debug.log")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)    
+
     logger.addHandler(file_handler)
     return logger
 
 
 def generate_passphrase(number_of_words:int = 6, seprator: str = '+') -> str:
 
-#    number1 = generate_random_number()
-#    number2 = generate_random_number()
-#    number3 = generate_random_number()
-#    number4 = generate_random_number()
-#    number5 = generate_random_number()
-#    number6 = generate_random_number()
-#
-#    word1 = choose_word_from_wordlist(number1)
-#    word2 = choose_word_from_wordlist(number2)
-#    word3 = choose_word_from_wordlist(number3)
-#    word4 = choose_word_from_wordlist(number4)
-#    word5 = choose_word_from_wordlist(number5)
-#    word6 = choose_word_from_wordlist(number6)
-
-
-#    passphrase =str(
-#                word1 + seprator + 
-#                word2 + seprator + 
-#                word3 + seprator +
-#                word4 + seprator +
-#                word5 + seprator +
-#                word6 + seprator 
-#                )
-
     ## Number of Words should be minimum of 3
-
     if (number_of_words < 3):
-        raise ValueError("number_of_words can not be less than 3")
+        raise ValueError("number_of_words can not be less than 3, Its really insecure")
 
-    passphrase = ""                 # Intialize Empty passphrase String
-
+    # Intialize Empty passphrase String
+    passphrase = ""                 
 
     for iteration in range(number_of_words):
-        number = generate_random_number()
+        # number = generate_random_number()
+        number = generate_cryptographically_secure_psuedo_random_number()
         word = choose_word_from_wordlist(number)
         passphrase = str(passphrase) + str(word)
         
@@ -55,18 +36,24 @@ def generate_passphrase(number_of_words:int = 6, seprator: str = '+') -> str:
         else:
             passphrase = str(passphrase) + str(seprator)
 
-
     return str(passphrase)
 
-def generate_random_number() -> int:
-    number = 0
-    for i in range(5):
-        number = str(number) + str(random.randint(1,6))
-    return int(number)
+
+def generate_cryptographically_secure_psuedo_random_number() -> int:
+    
+    # Faces of the Die that can be chosen at random
+    choice_list  = [1,2,3,4,5,6]
+
+    # 5 rolls to generate a 5-digit number like '15126'
+    digits = 5
+
+    rand_number = ''.join(str(secrets.choice(choice_list)) for _ in range(digits))
+
+    return int(rand_number)
 
 
 def choose_word_from_wordlist(number: int) -> str:
-    logger.debug(f"searching for word : {number}")
+    logger.debug(f"searching for word : <*****>")
     with open("wordlist.txt") as fp:
         for i, line in enumerate(fp):
             try:
@@ -79,13 +66,12 @@ def choose_word_from_wordlist(number: int) -> str:
     raise IndexError("choosen number out of bounds of wordlist")
 
 
-
-
-
-
+# Main Execution Block
 if __name__ == "__main__":
 
     logger = setup_logger()
+    
+    logger.debug("Creating Random PassPhrase")
 
     passphrase = generate_passphrase(number_of_words=6, seprator='$')
     print(passphrase)
